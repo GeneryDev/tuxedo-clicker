35f617af3e6fb155ed082bd2abab0cfa64384ce7
GeneryDev <generydev@gmail.com>
2026-07-08 14:37:10 -0400

    Add fast-forward debug command and macro

---
+++ scripts/CatClicker/GameStateManager Debug.cs
@@ -0,0 +1,14 @@
+﻿using GDF.Debug;
+
+namespace CatClicker;
+
+[HasDebugCommands]
+public partial class GameStateManager
+{
+    [DebugCommand("ffw")]
+    public static void FastForward()
+    {
+        var minutes = 5;
+        Instance.State.UnixTimestampSec -= (double)minutes * 60;
+    }
+}
