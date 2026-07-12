ffcecbeebee4a8542bcc40fa93134764703d6c84
GeneryDev <generydev@gmail.com>
2026-07-12 13:38:52 -0400

    Add sound effects

--- scripts/CatClicker/PointGeneratorFunctions.cs
+++ scripts/CatClicker/PointGeneratorFunctions.cs
@@ -5,16 +5,26 @@ namespace CatClicker;
 
 public partial class PointGeneratorFunctions : Node
 {
+    [Signal]
+    public delegate void PurchaseSuccessEventHandler();
+    [Signal]
+    public delegate void PurchaseFailedEventHandler();
+    
     [Export] public StringName GeneratorId = "";
 
     public void Purchase()
     {
         var state = new PointGeneratorStateContext(GeneratorId);
-        if (!state.CanAffordSinglePurchase()) return;
+        if (!state.CanAffordSinglePurchase())
+        {
+            EmitSignalPurchaseFailed();
+            return;
+        }
         int countToPurchase = state.CanAffordBulkPurchase() ? GameInterfaceManager.Instance.GetBulkPurchaseAmount() : state.GetAffordableBulkCount();
         int currentCount = state.GetCount();
         BigInteger totalCost = state.Definition.GetPointCostForLevels(currentCount, currentCount + countToPurchase);
         GameStateManager.Instance.AddGenerator(GeneratorId, countToPurchase);
         GameStateManager.Instance.WithdrawPoints(totalCost);
+        EmitSignalPurchaseSuccess();
     }
 }
--- scripts/CatClicker/UpgradeFunctions.cs
+++ scripts/CatClicker/UpgradeFunctions.cs
@@ -5,15 +5,25 @@ namespace CatClicker;
 
 public partial class UpgradeFunctions : Node
 {
+    [Signal]
+    public delegate void PurchaseSuccessEventHandler();
+    [Signal]
+    public delegate void PurchaseFailedEventHandler();
+    
     [Export] public StringName UpgradeId = "";
 
     public void Purchase()
     {
         var state = new UpgradeStateContext(UpgradeId);
-        if (!state.CanAffordPurchase()) return;
+        if (!state.CanAffordPurchase())
+        {
+            EmitSignalPurchaseFailed();
+            return;
+        }
         if (state.IsActive()) return;
         var cost = state.GetPurchaseCost();
         GameStateManager.Instance.AddUpgrade(UpgradeId);
         GameStateManager.Instance.WithdrawPoints(cost);
+        EmitSignalPurchaseSuccess();
     }
 }
